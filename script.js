d5c691580d0e5071026cfe031893bfa7dabae514
kusum <kusumlamichhane2015@gmail.com>
2020-12-16 17:20:35 +0545

    external javascript file

---
+++ script.js
@@ -0,0 +1,5 @@
+let js='amazing';
+        if (js==='amazing') alert('my fist js code is here');         
+          console.log(10+15-4*8);
+          console.log("kusum");
+          console.log(15+25-9);
