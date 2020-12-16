1537ad7a2376d47ae54127f250651bdab92470ca
kusum <kusumlamichhane2015@gmail.com>
2020-12-16 19:13:32 +0545

    learned  dynamic typing

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ let js='amazing';
 let firstName= "kusum";
 console.log(firstName);
 console.log(firstName);
+
+//boolean 
+ javascriptIsFun=true;
+ console.log(javascriptIsFun);
+ console.log(typeof true);
+ console.log(typeof 23);
+ console.log('jonas');
+
+
+ //dynamic typing --> easly changing the typle of value holded by variable
+
+
+ javascriptIsFun ='YEs!'
