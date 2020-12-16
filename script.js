0cb9df4676af2f4b44360d8354e74eeb4be26433
kusum <kusumlamichhane2015@gmail.com>
2020-12-16 18:59:48 +0545

    variable decleration

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let js='amazing';
           console.log(10+15-4*8);
           console.log("kusum");
           console.log(15+25-9);
+
+
+        //variable defining
+
+        //variable name start with small letters 
+
+let firstName= "kusum";
+console.log(firstName);
+console.log(firstName);
