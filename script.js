8309a580b622fdd9332e32997456146ed9fd3e4f
kusum <kusumlamichhane2015@gmail.com>
2020-12-18 16:18:32 +0545

    learned about variable decleration

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js='amazing';
+/*let js='amazing';
         if (js==='amazing') alert('my fist js code is here');         
           console.log(10+15-4*8);
           console.log("kusum");
@@ -24,4 +24,31 @@ console.log(firstName);
  //dynamic typing --> easly changing the typle of value holded by variable
 
 
- javascriptIsFun ='YEs!'
+ javascriptIsFun ='YES!';
+ console.log(typeof javascriptIsFun);
+
+ 
+
+*/
+ //decelering variable in js
+ //let ,const== ES6 ma aako  and var(old way)
+
+ //let can be change later -->for emptylai pani let use garne
+ let age =20;
+ age =30;//mutaed
+ console.log(age);
+
+
+ //value in const is never changed 
+ const birthYear =1999;
+ birthYear=1990;  
+
+
+ //const halda first mai value hunu parcha
+ //var is old and work as let 
+
+ var job ='programmer';
+ job='techer';
+
+ lastName ='lamichhane';
+ console.log(lastName);
