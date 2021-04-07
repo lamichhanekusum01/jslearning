8fc5db80b4fecc3d900040dfbc1675c32e22e4f3
kusumlamichhane01 <kusumlamichhane2015@gmail.com>
2021-04-07 19:24:00 +0545

    strict mode

---
+++ starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+let hasDriversLicense = true;
+const passTest = true;
+
+
+if(passTest) hasDriversLicense =true;
+if (hasDriversLicense) 
+console.log('I can drive ');
+
+const interface =audio;
