8a57c096743862a700990ea8e72bf5379072b52b
kusumlamichhane01 <kusumlamichhane2015@gmail.com>
2021-05-01 17:04:17 +0545

    function declerations and expression

--- starter/script.js
+++ starter/script.js
@@ -1,4 +1,4 @@
-'use strict';
+/* 'use strict';
 let hasDriversLicense = true;
 const passTest = true;
 
@@ -7,4 +7,36 @@ if(passTest) hasDriversLicense =true;
 if (hasDriversLicense) 
 console.log('I can drive ');
 
-const interface =audio;
+//const interface =audio; //private or resererved codes 
+
+
+// function 
+
+function fruitJuice (apple , orange)
+{
+    console.log(apple,orange);
+    const juice = `juice with ${orange} oranges and ${apple} apples`;
+    return juice ;
+}
+const FirstJuice = fruitJuice(1,2);
+console.log(FirstJuice);
+const SecondJuice = fruitJuice(5,8);
+console.log(SecondJuice); */
+
+
+
+//function decleration
+function calcAge1(birthYear)
+{
+    
+    return 2037-birthYear;
+}
+const age1 = calcAge1(1999);
+
+//ananomous function
+const calcAge2 =function (birthYear)
+{
+    return 2037-birthYear;
+}
+const age2 =calcAge2(1998); 
+console.log(age1,age2);
